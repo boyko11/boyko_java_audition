c74c8d537f358245da4a89fc2c34566e5c6f1c76
Boyko Todorov <boyko11@gmail.com>
2016-07-14 21:55:27 -0400

    When Pacman is directed to MOVE, Pacman's current direction and state change

--- src/main/java/com/audition/model/Pacman.java
+++ src/main/java/com/audition/model/Pacman.java
@@ -3,12 +3,51 @@ package com.audition.model;
 public class Pacman {
 
 	private Direction currentDirection;
+	private boolean moving;
 	
 	public Pacman() {
 		currentDirection = Direction.UP;
+		moving = false;
 	}
 	
 	public Direction getCurrentDirection() {
 		return currentDirection;
 	}
+	
+	public void setDirection(Direction direction) {
+		this.currentDirection = direction;
+	}
+	
+	public boolean isMoving() {
+		return moving;
+	}
+	
+	public void setMoving(boolean moving) {
+		this.moving = moving;
+	}
+	
+	public void moveUP() {
+		
+		this.moving = true;
+		this.currentDirection = Direction.UP; 
+	}
+
+	public void moveLEFT() {
+		
+		this.moving = true;
+		this.currentDirection = Direction.LEFT; 
+	}
+	
+	public void moveDOWN() {
+		
+		this.moving = true;
+		this.currentDirection = Direction.DOWN; 
+	}
+	
+	public void moveRIGHT() {
+		
+		this.moving = true;
+		this.currentDirection = Direction.RIGHT; 
+	}
+	
 }
--- src/test/java/com/audition/model/PacmanTest.java
+++ src/test/java/com/audition/model/PacmanTest.java
@@ -20,5 +20,63 @@ public class PacmanTest {
 		
 		assertEquals(Direction.UP, pacman.getCurrentDirection());
 	}
+	
+	@Test
+	public void test_WHEN_created_Pacman_SHOULD_NOT_be_moving() throws Exception {
+		
+		assertFalse(pacman.isMoving());
+	}
+	
+	@Test
+	public void test_moveUP_should_direct_UP_AND_move() throws Exception {
+		
+		Pacman pacmanUnderTest = setUpMoveTest(Direction.DOWN);
+		
+		pacmanUnderTest.moveUP();
+		
+		assertEquals(Direction.UP, pacmanUnderTest.getCurrentDirection());
+		assertTrue(pacmanUnderTest.isMoving());
+	}
+	
+	@Test
+	public void test_moveLEFT_should_direct_LEFT_AND_move() throws Exception {
+		
+		Pacman pacmanUnderTest = setUpMoveTest(Direction.UP);
+		
+		pacmanUnderTest.moveLEFT();
+		
+		assertEquals(Direction.LEFT, pacmanUnderTest.getCurrentDirection());
+		assertTrue(pacmanUnderTest.isMoving());
+	}
+	
+	@Test
+	public void test_moveDOWN_should_direct_DOWN_AND_move() throws Exception {
+		
+		Pacman pacmanUnderTest = setUpMoveTest(Direction.UP);
+		
+		pacmanUnderTest.moveDOWN();
+		
+		assertEquals(Direction.DOWN, pacmanUnderTest.getCurrentDirection());
+		assertTrue(pacmanUnderTest.isMoving());
+	}
+	
+	@Test
+	public void test_moveRIGHT_should_direct_RIGHT_AND_move() throws Exception {
+		
+		Pacman pacmanUnderTest = setUpMoveTest(Direction.UP);
+		
+		pacmanUnderTest.moveRIGHT();
+		
+		assertEquals(Direction.RIGHT, pacmanUnderTest.getCurrentDirection());
+		assertTrue(pacmanUnderTest.isMoving());
+	}
+	
+	private Pacman setUpMoveTest(Direction setupDirection) {
+		
+		Pacman pacmanUnderTest = new Pacman();
+		pacmanUnderTest.setDirection(setupDirection);
+		pacmanUnderTest.setMoving(false);
+		return pacmanUnderTest;
+	}
 
 }
